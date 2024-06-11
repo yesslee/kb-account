2b68f4fd11189ea40f7f19e429bb592be0a67912
dh1010a <dh1010a@naver.com>
2024-06-11 11:07:04 +0900

    feat: stores 기능 구현

---
+++ src/stores/transactions.js
@@ -0,0 +1,140 @@
+import { defineStore } from "pinia";
+import { reactive, computed } from 'vue';
+import axios from 'axios';
+
+const members = "members";
+const transaction = "transactionList";
+const BASEURI = "/api";
+
+export const useMemberStore = defineStore("memberStore", () => {
+    const state = reactive({ 
+        member:{}
+    });
+
+    const fetchMember = async (memberId) => {
+        if (!memberId || memberId.trim()==="") {
+            alert('멤버 ID는 반드시 입력해야 합니다');
+            return;
+        } 
+        try {
+            const response = await axios.get(BASEURI + `/${members}`);
+            if (response.status === 200) {
+                state.member = response.data.filter(member => member.memberId === memberId);
+            } else {
+                alert('멤버 조회 실패');
+            }
+        } catch(error) {
+            alert('에러발생 :' + error);
+        }
+    }
+
+    const updateMember = async ({ id, memberId, name, pw, email, gender }, successCallback) => {
+        if (!memberId || memberId.trim()==="") {
+            alert('멤버 ID는 반드시 입력해야 합니다');
+            return;
+        } 
+        try {
+            const payload = { memberId, name, pw, email, gender};
+            const response = await axios.put(BASEURI + `/${id}`, payload)
+            if (response.data.status === "success") {
+                state.member = { id, memberId, name, pw, email, gender };
+                successCallback();
+            } else {
+                alert('거래내역 변경 실패 : ' + response.data.message);
+            }
+        } catch(error) {
+            alert('에러발생 :' + error);
+        }
+    }
+    
+    const member = computed(()=>state.member);
+    return { member, fetchMember };
+});
+
+export const useTransactionStore = defineStore("transactionStore", () => {
+    const state = reactive({ 
+        transactionList : []
+    });
+
+    const fetchTransactionList = async (memberId) => {
+        try {
+            const response = await axios.get(BASEURI + `/${transaction}`);
+            if (response.status === 200) {
+                state.TranscationList = response.data.filter(transaction => transaction.memberId === memberId);
+            } else {
+                alert('데이터 조회 실패');
+            }
+        } catch(error) {
+            alert('에러발생 :' + error);
+        }
+    }
+
+    const addTransaction = async ({ memberId, date, price, category, type }, successCallback) => {
+        if (!memberId || memberId.trim()==="") {
+            alert('멤버 ID는 반드시 입력해야 합니다');
+            return;
+        } 
+        if (!date || date.trim()==="" || !price || price.trim()=="" || 
+        !category || category.trim()=="" || !type || type.trim()=="") {
+            alert('세부 항목은 반드시 입력해야 합니다');
+            return;
+
+        }
+        try {
+            const payload = { memberId, date, price, category, type};
+            const response = await axios.post(BASEURI, payload)
+            if (response.data.status === "success") {
+                state.todoList.push({memberId, date, price, category, type })
+                successCallback();
+              } else {
+                alert('거래내역 추가 실패 : ' + response.data.message);
+              }
+        } catch(error) {
+            alert('에러발생 :' + error);
+        }
+    }
+
+    const updateTransaction = async ({ id, memberId, date, price, category, type }, successCallback) => {
+        if (!memberId || memberId.trim()==="") {
+            alert('멤버 ID는 반드시 입력해야 합니다');
+            return;
+        } 
+        if (!date || date.trim()==="" || !price || price.trim()=="" || 
+        !category || category.trim()=="" || !type || type.trim()=="") {
+            alert('세부 항목은 반드시 입력해야 합니다');
+            return;
+
+        }
+        try {
+            const payload = { memberId, date, price, category, type };
+            const response = await axios.put(BASEURI + `/${id}`, payload)
+            if (response.data.status === "success") {
+                let index = state.transactionList.findIndex((transaction) => transaction.id === id);
+                state.transactionList[index] = { id, memberId, date, price, category, type };
+                successCallback();
+            } else {
+                alert('거래내역 변경 실패 : ' + response.data.message);
+            }
+        } catch(error) {
+            alert('에러발생 :' + error);
+        }
+    }
+
+    const deleteTransaction = async (id) => {
+        try {
+            const response = await axios.delete(BASEURI + `/${id}`)
+            if (response.data.status === "success") {
+                let index = state.transactionList.findIndex((transaction) => transaction.id === id);
+                state.transactionList.splice(index, 1);
+              } else {
+                alert('거래내역 삭제 실패 : ' + response.data.message);
+              }
+        } catch(error) {
+            alert('에러발생 :' + error);
+        }
+    }
+
+    const transactionList = computed(()=>state.transactionList);
+    
+    return { transactionList, fetchTransactionList, addTransaction, deleteTransaction, updateTransaction };
+});
