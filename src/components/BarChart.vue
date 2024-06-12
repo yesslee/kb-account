24d0efd55c734e92296baf63d0d22e4ea8a6ac15
lej8924 <eunjae8924@naver.com>
2024-06-12 19:42:07 +0900

    modify: pull을 위한 임시 커밋

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -9,13 +9,9 @@
   <script>
   import { Bar } from 'vue-chartjs'
   import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  import useTransactionStore from '@/stores/transactions.js'
+  import { useTransactionStore } from '@/stores/transactions.js'
   import { useRoute } from 'vue-router'
   import { ref, onMounted } from 'vue'
-
-  
-
-
 
   ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale) 
   
