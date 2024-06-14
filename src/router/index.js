cb3cfcc547fff8c6ceb0758a18f76afdcf1d2100
yesslee31 <yesslee31@gmail.com>
2024-06-14 10:11:55 +0900

    feat: 내비게이션 가드 기능 반영 및 로고 클릭 라우팅 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, isNavigationFailure } from "vue-router";
-// import { useAuthStore } from '@/stores/auth.js'
+import { useAuthStore } from '@/stores/auth.js'
 
 import Login from "@/pages/Login.vue";
 import UserInfo from "@/pages/UserInfo.vue";
@@ -23,26 +23,20 @@ const router = createRouter({
 });
 
 // 라우터 가드 설정
-// router.beforeEach((to, from, next) => {
-//   const authStore = useAuthStore()
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
 
-//   // // 중복된 네비게이션을 피하기 위해 현재 경로와 동일한 경로로의 이동을 무시합니다.
-//   // if (to.path === from.path) {
-//   //   next(false)
-//   //   return
-//   // }
-
-//   if (to.matched.some(record => record.meta.requiresAuth)) {
-//     // 로그인 상태 확인
-//     if (!authStore.isLoggedIn) {
-//       next({ name: 'login' }) // 로그인 페이지로 리다이렉트
-//     } else {
-//       next() // 로그인 상태면 접근 허용
-//     }
-//   } else {
-//     next() // requiresAuth가 없는 라우트는 항상 접근 허용
-//   }
-// })
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // 로그인 상태 확인
+    if (!authStore.isLoggedIn) {
+      next({ name: 'login' }) // 로그인 페이지로 리다이렉트
+    } else {
+      next() // 로그인 상태면 접근 허용
+    }
+  } else {
+    next() // requiresAuth가 없는 라우트는 항상 접근 허용
+  }
+})
 
 router.afterEach((to, from, failure) => {
   if (isNavigationFailure(failure)) {
